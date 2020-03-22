4b54389d0d42ebf2204c1b4254191c92b88306db
nischay.goyal <nischay.goyal@lloydsbanking.com>
2020-03-22 02:21:14 +0000

    bigtable script aded

--- common/projectSettings.sh
+++ common/projectSettings.sh
@@ -6,6 +6,15 @@ PROJECT_NAME="certification-project-271215"
 # Which default region should regional services use?
 PROJECT_REGION="europe-west2"
 
+# Which default zone should zonal services use
+PROJECT_ZONE="europe-west2-b"
+
+# Used as a suffix for select service names.
+ENV_TYPE="dev"
+
+# Used as a prefix for select service names.
+ORGANIZATION="find-seller"
+
 # The name of the custom network for the product and ads services
 SERVICES_NETWORK="certification-network"
 
@@ -25,3 +34,10 @@ PUBLIC_ASSETS="certification-public-bucket"
 
 # The name of the Pubsub topic to create / use
 PUB_SUB_TOPIC_NAME="certification-front-end"
+
+# Bigtable settings...
+# The name of our Bigtable instance. An instance is basically a container for our cluster.
+BIGTABLE_INSTANCE_ID="$ORGANIZATION-bt-instance-$ENV_TYPE"
+BIGTABLE_CLUSTER_ID="$ORGANIZATION-bt-cluster-$ENV_TYPE"
+BIGTABLE_DISPLAY_NAME="$ORGANIZATION-bt-name-$ENV_TYPE"
+BIGTABLE_TABLE_ID=items
